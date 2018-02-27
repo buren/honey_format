25cbc3dc04e9bd96b7109b4dac87556b7ec41b89
Jacob Burenstam <burenstam@gmail.com>
2018-02-27 11:23:17 +0100

    Remove space after closing )}] when converting a header

--- lib/honey_format/convert_header_value.rb
+++ lib/honey_format/convert_header_value.rb
@@ -4,10 +4,20 @@ module HoneyFormat
       [/ \(/, '('],
       [/ \[/, '['],
       [/ \{/, '{'],
+      [/\) /, ')'],
+      [/\] /, ']'],
+      [/\} /, '}'],
       [/ /, '_'],
       [/-/, '_']
     ].map { |array| array.freeze }.freeze
 
+    # Returns converted value and mutates the argument.
+    # @return [Symbol] the cleaned header column.
+    # @param [String] column the string to be cleaned.
+    # @example Convert simple header
+    #     ConvertHeaderValue.call("  User name ") #=> "user_name"
+    # @example Convert complex header
+    #     ConvertHeaderValue.call(" First name (user)") #=> :'first_name(user)'
     def self.call(column)
       column.strip!
       column.downcase!
