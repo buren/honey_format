edde29bb684cf5d04b5d8b61246b36daf2c8065a
Jacob Burenstam <burenstam@gmail.com>
2018-05-12 16:47:50 +0200

    Rows doc :lipstick:

--- lib/honey_format/rows.rb
+++ lib/honey_format/rows.rb
@@ -13,7 +13,7 @@ module HoneyFormat
       @rows = prepare_rows(Row.new(columns, builder: builder), rows)
     end
 
-    # @yield [row] The given block will be passed every row.
+    # @yield [row] The given block will be passed for every row.
     # @yieldparam [Row] a row in the CSV file.
     # @return [Enumerator]
     #   If no block is given, an enumerator object will be returned.
