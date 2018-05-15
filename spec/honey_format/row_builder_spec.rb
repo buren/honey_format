eeca149ac5d9f0bc4459879cd04eb01e2e547c4b
Jacob Burenstam <burenstam@gmail.com>
2018-05-15 14:34:52 +0200

    Add RowBuilder cell with newline char test

--- spec/honey_format/row_builder_spec.rb
+++ spec/honey_format/row_builder_spec.rb
@@ -28,6 +28,13 @@ describe HoneyFormat::RowBuilder do
       expect(person.to_csv).to eq("\"jacob,buren\",28\n")
     end
 
+    it 'handles strings containing a new line character' do
+      struct = described_class.call(%i[name age])
+      person = struct.new("jacob\nburen", 28)
+
+      expect(person.to_csv).to eq("\"jacob\nburen\",28\n")
+    end
+
     it 'calls #to_csv if supported for each of the members in the Struct' do
       struct = described_class.call(%i[name age])
       name_object = Class.new { define_method(:to_csv) { 'buren' } }.new
