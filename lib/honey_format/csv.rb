77d766f40927229b0d9b4e5628f6398663c7d7d4
Jacob Burenstam <burenstam@gmail.com>
2018-06-09 16:44:58 +0200

    Update documentation

--- lib/honey_format/csv.rb
+++ lib/honey_format/csv.rb
@@ -7,9 +7,11 @@ module HoneyFormat
   # Represents CSV.
   class CSV
     # @return [CSV] a new instance of CSV.
-    # @param [String] csv string.
-    # @param [Array<Symbol>] valid_columns valid array of symbols representing valid columns if empty all will be considered valid.
-    # @param [Array<String>] header optional argument for CSV header.
+    # @param [String] csv the CSV string
+    # @param [String] delimiter the CSV delimiter
+    # @param [Array<String>] header optional argument that represents CSV header, required if the CSV file lacks a header row.
+    # @param [Array<Symbol>] valid_columns array of symbols representing valid columns, if empty all will be considered valid.
+    # @param [#call] header_converter converts header columns.
     # @param [#call] row_builder will be called for each parsed row.
     # @raise [HeaderError] super class of errors raised when there is a CSV header error.
     # @raise [MissingHeaderError] raised when header is missing (empty or nil).
@@ -38,13 +40,12 @@ module HoneyFormat
     end
 
     # @return [Array] of rows.
-    # @raise [InvalidRowLengthError] raised when there are more row elements longer than columns
     def rows
       @rows
     end
 
     # @yield [row] The given block will be passed for every row.
-    # @yieldparam [Row] a column in the CSV header.
+    # @yieldparam [Row] row in the CSV.
     # @return [Enumerator] If no block is given, an enumerator object will be returned.
     def each_row
       return rows.each unless block_given?
@@ -54,8 +55,15 @@ module HoneyFormat
 
     # Convert CSV object as CSV-string.
     # @param columns [Array<Symbol>, Set<Symbol>, NilClass] the columns to output, nil means all columns (default: nil)
-    # @yieldparam [Row] each row - return truthy if you want the row to be included in the output
+    # @yield [row] The given block will be passed for every row - return truthy if you want the row to be included in the output
+    # @yieldparam [Row] row
     # @return [String] CSV-string representation.
+    # @example with selected columns
+    #   csv.to_csv(columns: [:id, :country])
+    # @example with selected rows
+    #   csv.to_csv { |row| row.country == 'Sweden' }
+    # @example with both selected columns and rows
+    #   csv.to_csv(columns: [:id, :country]) { |row| row.country == 'Sweden' }
     def to_csv(columns: nil, &block)
       @header.to_csv(columns: columns) + @rows.to_csv(columns: columns, &block)
     end
