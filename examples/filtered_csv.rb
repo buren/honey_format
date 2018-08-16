ac11c804deab0d26df6fe62bb1c381d650cedb88
Jacob Burenstam <burenstam@gmail.com>
2018-08-16 16:57:54 +0200

    Improve filter example

--- examples/filtered_csv.rb
+++ examples/filtered_csv.rb
@@ -4,14 +4,18 @@ require 'bundler/setup'
 require 'honey_format'
 
 csv_string = <<~CSV
-  email,name,age,country
-  john@example.com,John Doe,42,SE
-  jane@example.com,Jane Doe,42,DK
+  email,name,born,country
+  john@example.com,John,2000-03-03,SE
+  jane@example.com,Jane,1970-03-03,SE
+  chris@example.com,Chris,1980-03-03,DK
 CSV
 
 puts '== EXAMPLE: CSV output with filtered columns and rows =='
 puts
 puts '== CSV START =='
-csv = HoneyFormat::CSV.new(csv_string)
-puts csv.to_csv(columns: %i[age country]) { |row| row.country == 'SE' }
+csv = HoneyFormat::CSV.new(csv_string, type_map: { born: :date })
+csv_string = csv.to_csv(columns: %i[born country]) do |row|
+  row.country == 'SE' && row.born < Date.new(1990, 1, 1)
+end
+puts csv_string
 puts '== CSV END =='
