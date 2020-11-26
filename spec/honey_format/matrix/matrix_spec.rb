112047e9b778aa59b3cb2d93e37651c096fbd243
Jacob Burenstam <burenstam@gmail.com>
2020-11-26 08:52:36 +0000

    rubocop :lipstick:

--- spec/honey_format/matrix/matrix_spec.rb
+++ spec/honey_format/matrix/matrix_spec.rb
@@ -5,19 +5,19 @@ require 'spec_helper'
 describe HoneyFormat::Matrix do
   describe '#type_map' do
     it 'returns type map' do
-      data = [[:id, :name], ['1', 'jacob']]
+      data = [%i[id name], %w[1 jacob]]
       type_map = { id: :integer }
       matrix = described_class.new(data, type_map: type_map)
 
-      expect(matrix.type_map).to eq({ id: :integer })
+      expect(matrix.type_map).to eq(id: :integer)
     end
 
     it 'returns type map of only existing columns' do
-      data = [[:id, :name], ['1', 'jacob']]
+      data = [%i[id name], %w[1 jacob]]
       type_map = { id: :integer, identifier: :integer }
       matrix = described_class.new(data, type_map: type_map)
 
-      expect(matrix.type_map).to eq({ id: :integer })
+      expect(matrix.type_map).to eq(id: :integer)
     end
   end
 end
