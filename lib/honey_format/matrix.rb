e50c62a867df22fd199c22c5dd789b36cc02c86c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-29 13:23:45 +0800

    Delete lib directory

--- lib/honey_format/matrix.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'honey_format/matrix/row_builder'
-require 'honey_format/matrix/rows'
-require 'honey_format/matrix/row'
-require 'honey_format/matrix/header'
-require 'honey_format/matrix/matrix'
